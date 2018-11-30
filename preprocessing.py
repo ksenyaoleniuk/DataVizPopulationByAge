288cf8ddd810e81f934dcd74da585e3f57ab4c70
Оксана <ksenyaoleniuk@gmail.com>
2018-11-30 17:50:19 +0200

    modify preprocessing -> add String name of the legend, bin data

--- preprocessing.py
+++ preprocessing.py
@@ -8,18 +8,16 @@ df = pd.DataFrame(data=data)
 df = df[[(len(x) < 3) for x in df['age']]]
 
 #split data
-cols = ['men', 'women', 'age']
-df[cols] = df[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+cols = ['men', 'women','age']
+df[cols] = df[cols].apply(pd.to_numeric, downcast='integer', errors='coerce', axis=1)
 
 df['population'] = df['women'] + df['men']
-
 df = df.drop(['women', 'men'], axis=1)
 
 df.insert(loc=0, column='group', value="")
 
-df.loc[df['age'] > 65, 'group'] = "old"
-df.loc[df['age'] < 19, 'group'] = "young"
-df.loc[df['group'] == "", 'group'] = "middle"
-
+df.loc[df['age'] > 65, 'group'] = "65-80"
+df.loc[df['age'] < 19, 'group'] = "0-20"
+df.loc[df['group'] == "", 'group'] = "20-65"
 
 df.to_csv('preprocessed_population.csv', sep=',')
